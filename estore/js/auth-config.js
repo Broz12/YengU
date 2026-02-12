0995e11396ab37bf5ff792e40bd67269e3a3fc67
Brojendro Ningthoujam <xilyfewfew@gmail.com>
2026-02-13 02:54:33 +0530

    Switch auth config to Supabase legacy anon JWT key

--- estore/js/auth-config.js
+++ estore/js/auth-config.js
@@ -2,6 +2,6 @@
   // Configure these values from your Supabase project settings.
   window.YENGU_AUTH_CONFIG = {
     supabaseUrl: "https://qobqlrjsclykilxpvqzp.supabase.co",
-    supabaseAnonKey: "sb_publishable_AaRRdS-ycFUmxe-S4h_Rog_i2_0bXro",
+    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvYnFscmpzbGN5a2lseHB5cXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTcyMTMsImV4cCI6MjA4NjQ5MzIxM30.GdhsSNbDOzs41W4xkMqBK2M4p0YExYXu6kFZ-TyB0_k",
   };
 })();
