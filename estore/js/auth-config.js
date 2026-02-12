96bfbc533d25f2e42b0609bdc588b37912536412
Brojendro Ningthoujam <xilyfewfew@gmail.com>
2026-02-13 02:58:59 +0530

    Fix Supabase project URL to match anon key ref

--- estore/js/auth-config.js
+++ estore/js/auth-config.js
@@ -1,7 +1,7 @@
 (function () {
   // Configure these values from your Supabase project settings.
   window.YENGU_AUTH_CONFIG = {
-    supabaseUrl: "https://qobqlrjsclykilxpvqzp.supabase.co",
+    supabaseUrl: "https://qobqlrjslcykilxpyqzp.supabase.co",
     supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvYnFscmpzbGN5a2lseHB5cXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTcyMTMsImV4cCI6MjA4NjQ5MzIxM30.GdhsSNbDOzs41W4xkMqBK2M4p0YExYXu6kFZ-TyB0_k",
   };
 })();
