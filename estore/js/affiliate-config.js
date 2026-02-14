cb381ec27b59660b7c49ceebc719ff6eac415f5f
Brojendro Ningthoujam <xilyfewfew@gmail.com>
2026-02-14 20:39:42 +0530

    Add Trends category page with live Amazon products and clean category navigation

--- estore/js/affiliate-config.js
+++ estore/js/affiliate-config.js
@@ -11,12 +11,24 @@
       "yg-sable-noir": "sable noir satin dress",
       "yg-velour-arc": "velour arc luxury handbag",
       "yg-ivory-grid": "ivory grid luxury handbag",
+      "yg-trend-mamaearth": "mamaearth ubtan natural glow face wash turmeric saffron",
+      "yg-trend-meemee": "mee mee wet wipes baby skin aloe vera",
+      "yg-trend-maybelline": "maybelline new york matte pencil eyeliner",
+      "yg-trend-littles": "little soft cleansing baby wipes with lid",
+      "yg-trend-skinn-ibiza": "skinn by titan tales ibiza perfume",
+      "yg-trend-plum": "plum e-luminence deep moisturizing creme vitamin e jojoba oil",
     },
     // Optional direct product overrides by key "platform:productId".
     linkOverrides: {
       "amazon:yg-aurora-silk-gown": "https://amzn.in/d/08Q9xMwW",
       "amazon:yg-aurora-red": "https://amzn.in/d/03r1WIcE",
       "amazon:yg-aurora-blue": "https://amzn.in/d/08Q9xMwW",
+      "amazon:yg-trend-mamaearth": "https://amzn.in/d/05T9lzmM",
+      "amazon:yg-trend-meemee": "https://amzn.in/d/0dKHNQ2e",
+      "amazon:yg-trend-maybelline": "https://amzn.in/d/00FZfy1K",
+      "amazon:yg-trend-littles": "https://amzn.in/d/0j7Uun8i",
+      "amazon:yg-trend-skinn-ibiza": "https://amzn.in/d/097cpfz3",
+      "amazon:yg-trend-plum": "https://amzn.in/d/0f7Swn2l",
     },
   };
 })();
